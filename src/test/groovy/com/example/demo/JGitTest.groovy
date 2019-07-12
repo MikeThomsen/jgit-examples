ab59daf73ebf1b9c1ca1ff63d81e0210ccfaa68f
Mike Thomsen <mikerthomsen@gmail.com>
2019-07-12 09:08:17 -0400

    branch test works.

--- src/test/groovy/com/example/demo/JGitTest.groovy
+++ src/test/groovy/com/example/demo/JGitTest.groovy
@@ -1,11 +1,17 @@
 package com.example.demo
 
 import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.lib.Repository
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder
 import org.junit.Test
 
 class JGitTest {
+    def temp = File.createTempDir()
+    Repository repo
     Git createEmptyRepo() {
-        Git.init().setDirectory(File.createTempDir()).call()
+        repo = FileRepositoryBuilder.create(new File(temp, ".git"))
+        repo.create()
+        new Git(repo)
     }
 
     @Test
@@ -18,15 +24,28 @@ class JGitTest {
     @Test
     void testCreateBranchWithCommit() {
         def git = createEmptyRepo()
-        def folder = git.repository.directory
-        new File(folder, "README.md").write("""
-            # Hello World!
-        """)
-        git.add().addFilepattern("*.md").call()
-        git.commit().setMessage("First commit").call()
+        def commit = { message ->
+            new File(temp, "README.md").write("""
+                # Hello World!
+            """)
+            git.add().addFilepattern("*.md").call()
+            git.commit().setMessage(message).call()
+        }
+        commit("First commit")
         git.branchCreate().setName("dev").call()
+        git.checkout().setName("dev").call()
         def list = git.branchList().call()
         assert list.find { it.name.endsWith("master") }
         assert list.find { it.name.endsWith("dev") }
+        commit("Second commit")
+        def dev = git.log().all().call()
+        assert dev.size() == 2
+        git.checkout().setName("master").call()
+        def master = git.log().call()
+        assert repo.branch == "master"
+        def masterCommits = []
+        master.each { masterCommits << it }
+        assert masterCommits.size() == 1
+        assert masterCommits[0].fullMessage == 'First commit'
     }
 }
