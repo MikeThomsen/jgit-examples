6e4d18cddbc5cda30a845c71400f1ba57fd497f0
Mike Thomsen <mikerthomsen@gmail.com>
2019-07-12 08:43:15 -0400

    Added testCreateBranchWithCommit.

--- src/test/groovy/com/example/demo/JGitTest.groovy
+++ src/test/groovy/com/example/demo/JGitTest.groovy
@@ -4,10 +4,29 @@ import org.eclipse.jgit.api.Git
 import org.junit.Test
 
 class JGitTest {
+    Git createEmptyRepo() {
+        Git.init().setDirectory(File.createTempDir()).call()
+    }
+
     @Test
     void testCreateRepo() {
-        def git = Git.init().setDirectory(File.createTempDir()).call()
+        def git = createEmptyRepo()
         def status = git.status().call()
         assert status.clean
+    }
+
+    @Test
+    void testCreateBranchWithCommit() {
+        def git = createEmptyRepo()
+        def folder = git.repository.directory
+        new File(folder, "README.md").write("""
+            # Hello World!
+        """)
+        git.add().addFilepattern("*.md").call()
+        git.commit().setMessage("First commit").call()
+        git.branchCreate().setName("dev").call()
+        def list = git.branchList().call()
+        assert list.find { it.name.endsWith("master") }
+        assert list.find { it.name.endsWith("dev") }
     }
 }
